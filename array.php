9f7a35db54c04bc670bb95c5cb893c493d4f996e
RoyLin <orangeskyonline@gmail.com>
2024-10-22 13:58:17 +0800

    用陣列寫九九乘法表

--- array.php
+++ array.php
@@ -227,6 +227,249 @@ echo "</pre>";
     }
     ?>
 </table>
+
+<h2>利用程式來產生陣列</h2>
+
+<ul>
+     <li>以迴圈的方式產生一個九九乘法表</li>
+    <li>將九九乘法表的每個項目以字串型式存入陣列中</li>
+    <li>再以迴圈方式將陣列內容印出</li>
+</ul>
+
+<?php
+$nine=[];
+
+for ($i=1; $i<=9; $i++) { 
+    for ($j=1; $j<=9; $j++) { 
+        $nine[]="$i x $j = " . ($i*$j);
+        // echo "$i x $j = ";
+        // echo $i*$j;
+        // echo ",";
+    }
+}
+
+// echo "<pre>";
+// // <pre>標籤裡的資料用原本的格式來呈現
+// print_r($nine);
+// echo "<pre>"
+
+
+/* 另一種斷行的方法
+    $counter=0;
+    foreach($nine as $idx => $n){
+    $counter++;
+    if($counter==9){
+        echo $n . "<br>";
+        $counter=0;
+    }else{
+        echo $n . ",";
+    
+    }
+} */
+
+
+foreach($nine as $idx => $n)
+    if(($idx + 1) % 9 == 0){
+        echo $n . "<br>";
+    }else{
+        echo $n . ",";
+}
+
+
+   /*  echo "<table>";
+    for ($i=1; $i <=9; $i++) { 
+    echo "<tr>";
+    for ($j=1; $j <=9; $j++) { 
+        echo "<td>";
+         echo "$i x $j =";
+         echo $i*$j;
+         echo "</td>";
+    }
+}  */
+
+
+
+?>
+<hr>
+<?php
+// 完整版
+$nine = []; // 建立一個空陣列來儲存乘法表
+
+// 使用兩層迴圈生成九九乘法表
+for ($i = 1; $i <= 9; $i++) { 
+    for ($j = 1; $j <= 9; $j++) { 
+        $nine[] = "$i x $j = " . ($i * $j); // 將每個項目存入陣列
+    }
+}
+
+// 使用迴圈印出陣列的內容
+foreach ($nine as $idx => $n) {
+    if (($idx + 1) % 9 == 0) {
+        echo $n . "<br>"; // 每 9 個項目換行
+    } else {
+        echo $n . ", "; // 其他項目之間用逗號分隔
+    }
+}
+
+?>
+<hr>
+<?php
+$nine = []; // 建立一個空陣列來儲存乘法表
+
+// 使用兩層迴圈生成九九乘法表
+for ($i = 1; $i <= 9; $i++) { 
+    for ($j = 1; $j <= 9; $j++) { 
+        $nine[] = "$i x $j = " . ($i * $j); // 將每個項目存入陣列
+    }
+}
+
+// 開始建立 HTML 表格
+echo "<table border='1' style='border-collapse:collapse;'>";
+echo "<tr>"; // 開始一行
+
+foreach ($nine as $idx => $n) {
+    echo "<td>$n</td>"; // 每個乘法項目放入一個表格單元格
+
+    // 每 9 個項目換行
+    if (($idx + 1) % 9 == 0) {
+        echo "</tr>"; // 關閉當前行
+        if ($idx + 1 < count($nine)) {
+            echo "<tr>"; // 開始新的一行
+        }
+    }
+}
+
+echo "</table>"; // 關閉表格
+?>
+<?php
+$nine = []; // 建立一個空陣列來儲存乘法表
+
+// 使用兩層迴圈生成九九乘法表
+for ($i = 1; $i <= 9; $i++) { 
+    for ($j = 1; $j <= 9; $j++) { 
+        $result = $i * $j; // 計算結果
+        $nine[] = "$result"; // 僅將計算結果存入陣列
+    }
+}
+
+// 開始建立 HTML 表格
+echo "<table border='1' style='border-collapse:collapse;'>";
+echo "<tr>"; // 開始一行
+
+foreach ($nine as $idx => $n) {
+    echo "<td>$n</td>"; // 每個計算結果放入一個表格單元格
+
+    // 每 9 個項目換行
+    if (($idx + 1) % 9 == 0) {
+        echo "</tr>"; // 關閉當前行
+        if ($idx + 1 < count($nine)) {
+            echo "<tr>"; // 開始新的一行
+        }
+    }
+}
+
+echo "</table>"; // 關閉表格
+?>
+
+<!-- <?php
+// 老師的寫法
+$nine=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $nine[]="$i x $j = ".($i*$j);
+    }    
+}
+
+echo "<table>";
+foreach($nine as $idx => $n){
+    $v=explode("=",$n)[1];
+    if($idx%9==8){
+        echo "<td>$v</td></tr>";
+
+    }else if($idx%9==0){
+        echo "<tr><td>$v</td>";
+
+    }else{
+        echo "<td>$v</td>";
+    }
+}
+echo "</table>";
+
+
+?> -->
+<?php
+echo "<hr>";
+$nine2=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $nine2["$i x $j"]=$i*$j;
+    }    
+}
+
+echo "<pre>";
+print_r($nine2);
+echo "</pre>";
+
+echo "<hr>";
+
+$counter=0;
+foreach($nine2 as $key => $n){
+    $counter++;
+    if($counter==9){
+        echo $key  . " = ".$n ."<br>";
+        $counter=0;
+    }else{
+        echo $key  . " = ".$n .", ";
+
+    }
+}
+echo "<hr>";
+
+echo "<table>";
+$counter=0;
+foreach($nine2 as $n){
+    $counter++;
+    if($counter==9){
+        echo "<td>$n</td></tr>";
+        $counter=0;
+    }else if($counter==1){
+        echo "<tr><td>$n</td>";
+
+    }else{
+        echo "<td>$n</td>";
+    }
+}
+echo "</table>";
+echo "<hr>";
+echo "<hr>";
+$nine3=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $nine3[]=['formula' => "$i x $j",'value'=>$i*$j];
+    }    
+}
+
+echo "<pre>";
+print_r($nine3);
+echo "</pre>";
+
+echo "<hr>";
+
+foreach($nine3 as $idx =>$item){
+    if($idx%9==8){
+        echo $item['formula'] . " = ".$item['value']. "<br>";
+    }else{
+        echo $item['formula'] . " = ".$item['value'];
+
+    }
+}
+
+?>
+
+
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
